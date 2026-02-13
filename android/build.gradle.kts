60ffa73fd41037cd6db9f87b7442906ce90c5951
mohamed-zaitoon <mohamedzaitoon01@gmail.com>
2026-02-13 12:21:15 +0200

    chore(android): suppress build warnings and disable gradle file watching

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,5 @@
 // Open-source code. Copyright Mohamed Zaitoon 2025-2026.
+import org.gradle.api.tasks.compile.JavaCompile
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -22,6 +23,21 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        options.isWarnings = false
+        options.isDeprecation = false
+        options.compilerArgs.addAll(
+            listOf(
+                "-nowarn",
+                "-Xlint:-options",
+                "-Xlint:-unchecked",
+                "-Xlint:-deprecation"
+            )
+        )
+    }
 }
 
 tasks.register<Delete>("clean") {
